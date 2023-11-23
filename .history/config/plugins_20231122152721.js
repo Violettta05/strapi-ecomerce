936667459cea43b9f31cb5052ead09d9a08a67eb
Violettta05 <davidrey20@outlook.es>
2023-11-23 18:23:32 -0500

    my backend almost ready

---
+++ .history/config/plugins_20231122152721.js
@@ -0,0 +1,19 @@
+module.exports = () => ({
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          s3Options: {
+            accessKeyId: 'AKIA5ZOTNGW6HGIKCX5P',
+            secretAccessKey: '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',
+            region: 'us-east-1#',
+            params: {
+              ACL: 'public-read',
+              signedUrlExpires:  15 * 60,
+              Bucket: 'g-winkle-ecomerce',
+            },
+          },
+        },
+      },
+    },
+  });
